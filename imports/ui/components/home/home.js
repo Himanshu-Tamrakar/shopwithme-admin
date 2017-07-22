8accd9581191b43fe841ea3029c3c8d33ce8df82
Aman Gupta <aman.gupta@yourmarch.com>
2017-07-22 23:52:59 +0530

    Final Commit

--- imports/ui/components/home/home.js
+++ imports/ui/components/home/home.js
@@ -7,60 +7,60 @@ import {
     Meteor
 } from 'meteor/meteor';
 import {
-  name as HeaderImages
+    name as HeaderImages
 } from '../headerImages/headerImages'
 import {
-  name as AllProducts
+    name as AllProducts
 } from '../allProducts/allProducts'
 import {
-  name as AllCardObjects
+    name as AllCardObjects
 } from '../allCardObjects/allCardObjects'
 import {
-  name as QuestionAndAnswers
+    name as QuestionAndAnswers
 } from '../questionAndAnswers/questionAndAnswers'
 
 class Home {
-  constructor($scope, $reactive, $timeout, $state, $q) {
-    'ngInject';
+    constructor($scope, $reactive, $timeout, $state, $q) {
+        'ngInject';
 
-    $reactive(this).attach($scope);
+        $reactive(this).attach($scope);
 
-    this.scope = $scope;
-    this.timeout = $timeout;
-    this.state = $state;
-  }
+        this.scope = $scope;
+        this.timeout = $timeout;
+        this.state = $state;
+    }
 }
 
 
 const name = 'home';
 
 export default angular.module(name, [
-  angularMeteor,
-  uiRouter,
-  ngMaterial,
-  HeaderImages,
-  AllProducts,
-  AllCardObjects,
-  QuestionAndAnswers
+    angularMeteor,
+    uiRouter,
+    ngMaterial,
+    HeaderImages,
+    AllProducts,
+    AllCardObjects,
+    QuestionAndAnswers
 ]).component(name, {
-  template,
-  controllerAs: name,
-  controller: Home
+    template,
+    controllerAs: name,
+    controller: Home
 }).config(config);
 
 function config($stateProvider) {
-  'ngInject';
-  $stateProvider.state('home', {
-    url: '/home',
-    template: '<home></home>',
-    resolve: {
-      currentUser($q, $state) {
-        if (Meteor.userId() === null) {
-          $state.go('login')
-        } else {
-          return $q.resolve();
+    'ngInject';
+    $stateProvider.state('home', {
+        url: '/home',
+        template: '<home></home>',
+        resolve: {
+            currentUser($q, $state) {
+                if (Meteor.userId() === null) {
+                    $state.go('login')
+                } else {
+                    return $q.resolve();
+                }
+            }
         }
-      }
-    }
-  });
+    });
 }
